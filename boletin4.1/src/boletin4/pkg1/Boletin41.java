63ac46fb63644bdf04f9219ccd0a222fe8a223f7
antonio <antonio@biruseto>
2016-10-20 12:03:01 +0200

    boletin4.1hecho

---
+++ boletin4.1/src/boletin4/pkg1/Boletin41.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin4.pkg1;
+
+
+public class Boletin41 {
+
+    public static void main(String[] args) {
+     Coche car1=new Coche();
+     car1.acelerar(100);
+     car1.acelerar(30);
+     car1.frenar(10); 
+     
+      
+    }
+}
